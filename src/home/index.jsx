748dea2f1e8d7dd41bde2a5c41d8f188f6f43dda
mfarrukh14 <87035938+mfarrukh14@users.noreply.github.com>
2024-08-08 23:40:27 +0500

    Added  header as well as clerk authorisation

--- src/home/index.jsx
+++ src/home/index.jsx
@@ -1,8 +1,13 @@
+import Header from '@/components/custom/Header'
+import { UserButton, useUser } from '@clerk/clerk-react'
 import React from 'react'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <Header />
+      Landing Page
+    </div>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -7,20 +7,26 @@ import SignInPage from './auth/sign-in/index.jsx'
 import Home from './home/index.jsx'
 import path from 'path'
 import Dashboard from './dashboard/index.jsx'
+import { ClerkProvider } from '@clerk/clerk-react'
 
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key")
+}
 const router = createBrowserRouter([
   {
     element:<App/>,
     children: [
       {
-        path: '/',
-        element:<Home />
-      },
-      {
         path: '/dashboard',
         element:<Dashboard />
       }
     ]
+  },
+  {
+    path: '/',
+    element:<Home />
   },
   {
     path: '/auth/sign-in',
@@ -30,6 +36,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+  <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
     <RouterProvider router={router} />
+    </ClerkProvider>
   </React.StrictMode>,
 )
